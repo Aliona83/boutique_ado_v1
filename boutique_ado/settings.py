0a81c73eee0662ba68ec01670eb006c243bc670f
Aliona83 <alionajanusko@gmail.com>
2023-07-07 13:55:26 +0000

    Add env.py file for stripe_keys

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists('env.py'):
+    import env
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rna-7zo!1uar#yf!34p9th^=d1fhpn*c)@8p0a9j1t88p7%s$d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
